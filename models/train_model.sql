e47f3dee56ef127d4a7a1ea94eb06ef8041f378f
Ry02024 <162527496+Ry02024@users.noreply.github.com>
2024-05-04 15:15:12 +0900

    Update train_model.sql

--- models/train_model.sql
+++ models/train_model.sql
@@ -1,1 +1,15 @@
-
+#standardSQL
+CREATE OR REPLACE MODEL `your_project.your_dataset.crime_prediction_model`
+OPTIONS(model_type='logistic_reg') AS
+SELECT
+  primary_type,
+  EXTRACT(HOUR FROM TIMESTAMP(timestamp)) AS hour_of_day,
+  EXTRACT(DAYOFWEEK FROM TIMESTAMP(timestamp)) AS day_of_week,
+  COUNT(*) AS num_crimes,
+  IF(COUNT(*) > 10, 1, 0) AS label
+FROM
+  `your_dataset.your_crime_table`
+WHERE
+  TIMESTAMP(timestamp) BETWEEN '2014-01-01' AND '2014-06-30'
+GROUP BY
+  hour_of_day, day_of_week, primary_type;
